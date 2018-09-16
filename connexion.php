f6b4fcf583e1d264f39403183b8650f8061668cc
Gabriel <gabb_gregoire@hotmail.com>
2018-09-16 13:05:17 -0400

    formulaire connexion

--- connexion.php
+++ connexion.php
@@ -5,11 +5,45 @@
  * Date: 2018-09-16
  * Time: 10:03
  */
+require('requetes.php');
+
+
+if(isset($_POST)) {
+    $users = mysqli_query($conn, $selectUsers);
+    $uname = $_POST['courriel'];
+    $passw = md5($_POST['pwd']);
+
+    while($row = mysqli_fetch_assoc($users)) {
+        if($row['email'] == $uname) {
+            if($row['pword'] == $passw) {
+                header("Location: catalogue.php");
+            }
+            else {
+                alert("Mot de Passe Incorrect");
+
+                function alert($msg) {
+                    echo "<script type='text/javascript'>alert('$msg');</script>";
+                }
+            }
+        } else {
+            alert("Courriel Incorrect");
+
+            function alert($msg) {
+                echo "<script type='text/javascript'>alert('$msg');</script>";
+            }
+        }
+    }
+
+}
+
 ?>
 
 <div class="connect">
-    <label for="email">Entrez votre Courriel</label><br />
-    <input type="text" name="courriel" id="email"><br />
-    <label for="pwd">Entrez votre mot de passe</label><br />
-    <input type="text" name="pwd" id="pwd">
+    <form method="post" action="<?= $_SERVER["PHP_SELF"]?>">
+        <label for="email">Entrez votre Courriel</label><br />
+        <input type="text" name="courriel" id="email"><br />
+        <label for="pwd">Entrez votre mot de passe</label><br />
+        <input type="text" name="pwd" id="pwd">
+        <input type="submit" value="Connection">
+    </form>
 </div>
