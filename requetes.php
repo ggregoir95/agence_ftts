19a2e66264eca4c03d2f13018b8b63f353a920e9
Gabriel <gabb_gregoire@hotmail.com>
2018-09-15 22:55:33 -0400

    update reqs

--- requetes.php
+++ requetes.php
@@ -5,3 +5,21 @@
  * Date: 2018-09-12
  * Time: 08:13
  */
+
+$servername = "localhost";
+$username = "root";
+$password = "abc123...";
+$dbname = "db_ftts";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+mysqli_set_charset($conn,"utf8");
+$selectDestinations = "SELECT * FROM destinations";
+$selectCategs = "SELECT * FROM categorie";
+$selectUsers = "SELECT * FROM user";
+
+//$result = $conn->query($sql);
